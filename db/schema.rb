e740278848e171c8edbcb27898e6e4f21eae263d
tsaarikivi <tero.saarikivi@outlook.com>
2016-02-22 16:46:34 +0200

    Add user with email

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222134010) do
+ActiveRecord::Schema.define(version: 20160222144253) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
