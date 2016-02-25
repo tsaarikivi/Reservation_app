6cf7fffedd9b13b69742a0d13d738ef71cf2dacf
tsaarikivi <tero.saarikivi@outlook.com>
2016-02-25 17:24:08 +0200

    Add housing creation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :housings, dependent: :destroy
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223141403) do
+ActiveRecord::Schema.define(version: 20160225124136) do
+
+  create_table "housings", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "housings", ["user_id", "created_at"], name: "index_housings_on_user_id_and_created_at"
+  add_index "housings", ["user_id"], name: "index_housings_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
