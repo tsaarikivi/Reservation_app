e787412d43b02c55bb2345cb858a9e5190f30327
saarikiv <tuomo.saarikivi@microsoft.com>
2016-03-01 13:08:39 +0200

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed data for Reservation_app
+# Data aims to describe the Hakolahdentie 2 sauna and dryer.
+
+
+isannoitsija = Admin.create(
+    name: 'Liisa Anttila',
+    email: 'liisa.anttila@isannointisaarinen.fi',
+    password: 'salasana',
+    password_confirmation: 'salasana'
+    )
+
+omistaja = Owner.create(
+    name: 'As Oy Hakolahdentie 2', 
+    admin_id: isannoitsija.admin_id
+    )
+
+sauna = ReservationTarget.create(
+    name: 'Sauna', 
+    timeWindowInDays: 7, 
+    category: 1
+    )
+    
+kuivaushuone = ReservationTarget.create(
+    name: 'Kuivaushuone', 
+    timeWindowInDays: 3, 
+    category: 2
+    )
+
+A1 = User.create({name: 'A1', owner_id: omistaja.owner_id})
+   # A1_T1 = Token.create(user_id: A1.user_id, reservationTarget_id: sauna.reservationTarget_id)
+   # A1_T2 = Token.create(user_id: A1.user_id, reservationTarget_id: kuivaushuone.reservationTarget_id)
+C25 = User.create({name: 'C25', owner_id: omistaja.owner_id})
+   # C25_T1 = Token.create(user_id: C25.user_id, reservationTarget_id: sauna.reservationTarget_id)
+   # C25_T2 = Token.create(user_id: C25.user_id, reservationTarget_id: kuivaushuone.reservationTarget_id)
+    
+
+# Ma18_19 = Slot.create(reservationTarget_id: sauna.reservationTarget_id, day: 1, startTime: 1800, endTime: 1900)    
+# Ma19_20 = Slot.create(reservationTarget_id: sauna.reservationTarget_id, day: 1, startTime: 1900, endTime: 2000, token_id: A1_T1.token_id)    
+# Ma20_21 = Slot.create(reservationTarget_id: sauna.reservationTarget_id, day: 1, startTime: 2000, endTime: 2100)    
+# Ti18_19 = Slot.create(reservationTarget_id: sauna.reservationTarget_id, day: 2, startTime: 1800, endTime: 1900)    
+# Ti19_20 = Slot.create(reservationTarget_id: sauna.reservationTarget_id, day: 2, startTime: 1900, endTime: 2000)    
+# Ti20_21 = Slot.create(reservationTarget_id: sauna.reservationTarget_id, day: 2, startTime: 2000, endTime: 2100)    
+# To20_21 = Slot.create(reservationTarget_id: sauna.reservationTarget_id, day: 4, startTime: 2000, endTime: 2100, token_id: C25_T1.token_id)
+    
+    
