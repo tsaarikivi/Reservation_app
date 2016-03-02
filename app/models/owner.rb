818571d92f7479d4f585955e8555a841df19aa8f
tsaarikivi <tero.saarikivi@outlook.com>
2016-03-02 11:29:04 +0200

    schema & usrmodel

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,7 @@
 class Owner < ActiveRecord::Base
   belongs_to :admin
   has_many :reservation_targets, dependent: :destroy
+  has_many :users, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :admin_id, presence: true
   validates :name, presence: true, length: { maximum: 100 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301152148) do
+ActiveRecord::Schema.define(version: 20160302091713) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email"
